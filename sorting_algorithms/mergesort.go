abc31d59fbb426f1d0c0b9d71028d28942757bb8
thomasschafer <thomasschafer97@gmail.com>
2022-02-12 12:22:02 +0000

    Implemented Mergesort

--- sorting_algorithms/mergesort.go
+++ sorting_algorithms/mergesort.go
@@ -1,5 +1,39 @@
 package sorting_algorithms
 
 func Mergesort(arr []int) {
+	if len(arr) <= 1 {
+		return
+	}
 
+	var mid int = len(arr) / 2
+	left := append([]int{}, arr[:mid]...)
+	right := append([]int{}, arr[mid:]...)
+
+	Mergesort(left)
+	Mergesort(right)
+
+	idxLeft, idxRight, idx := 0, 0, 0
+
+	for idxLeft < len(left) && idxRight < len(right) {
+		if left[idxLeft] < right[idxRight] {
+			arr[idx] = left[idxLeft]
+			idxLeft++
+		} else {
+			arr[idx] = right[idxRight]
+			idxRight++
+		}
+		idx++
+	}
+
+	for idxLeft < len(left) {
+		arr[idx] = left[idxLeft]
+		idxLeft++
+		idx++
+	}
+
+	for idxRight < len(right) {
+		arr[idx] = right[idxRight]
+		idxRight++
+		idx++
+	}
 }
--- sorting_algorithms/sorting_test.go
+++ sorting_algorithms/sorting_test.go
@@ -7,18 +7,28 @@ import (
 func arraysAndSorted() [][][]int {
 	return [][][]int{
 		{{3, 5, 2}, {2, 3, 5}},
+		{{}, {}},
+		{{1, 2, 3, 4}, {1, 2, 3, 4}},
+		{{-1, -2, -3, -4}, {-4, -3, -2, -1}},
+		{{99, -11, 5, 2, 19, 37, -1000}, {-1000, -11, 2, 5, 19, 37, 99}},
 	}
 }
 
 func TestQuicksort(t *testing.T) {
 	for _, arrayAndSorted := range arraysAndSorted() {
 		array := arrayAndSorted[0]
+		arrayOriginal := append([]int{}, array...)
 		sorted := arrayAndSorted[1]
 		Quicksort(array)
 
 		for idx := range array {
 			if array[idx] != sorted[idx] {
-				t.Errorf("Quicksort failed to sort array %v", array)
+				t.Errorf(
+					"Quicksort failed to sort array %v.\nExpected %v\nRecieved %v",
+					arrayOriginal,
+					sorted,
+					array,
+				)
 				break
 			}
 		}
@@ -28,12 +38,18 @@ func TestQuicksort(t *testing.T) {
 func TestHeapsort(t *testing.T) {
 	for _, arrayAndSorted := range arraysAndSorted() {
 		array := arrayAndSorted[0]
+		arrayOriginal := append([]int{}, array...)
 		sorted := arrayAndSorted[1]
 		Heapsort(array)
 
 		for idx := range array {
 			if array[idx] != sorted[idx] {
-				t.Errorf("Heapsort failed to sort array %v", array)
+				t.Errorf(
+					"Heapsort failed to sort array %v.\nExpected %v\nRecieved %v",
+					arrayOriginal,
+					sorted,
+					array,
+				)
 				break
 			}
 		}
@@ -43,12 +59,18 @@ func TestHeapsort(t *testing.T) {
 func TestMergesort(t *testing.T) {
 	for _, arrayAndSorted := range arraysAndSorted() {
 		array := arrayAndSorted[0]
+		arrayOriginal := append([]int{}, array...)
 		sorted := arrayAndSorted[1]
 		Mergesort(array)
 
 		for idx := range array {
 			if array[idx] != sorted[idx] {
-				t.Errorf("Mergesort failed to sort array %v", array)
+				t.Errorf(
+					"Mergesort failed to sort array %v.\nExpected %v\nRecieved %v",
+					arrayOriginal,
+					sorted,
+					array,
+				)
 				break
 			}
 		}
